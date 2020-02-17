8173dfecc4b49be6d2059a39663968887f91c732
James Michael Armstrong(BlazesRus) <BlazesRus@yahoo.com>
2020-02-16 19:24:03 -0500

    Set up some initial files

---
+++ Resource.h
@@ -0,0 +1,27 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by MultiView.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_MULTIVTYPE                  129
+#define ID_VIEW_PRIMARYVIEW             32771
+#define ID_VIEW_SECONDARYVIEW           32772
+#define IDM_ABOUTBOX                    0x0010
+#define IDS_ABOUTBOX                    101
+
+#define IDD_NODETEXT                    129
+#define IDC_ST_TREE                     1000
+#define IDC_EF_TEXT                     1001
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
